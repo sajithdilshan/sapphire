0c96319a774f64c12bf3d0e2d993121a8c3c06bc
Sajith Dilshan <sajithdilshan@gmail.com>
2013-08-31 12:52:42 +0530

    changed auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     ENV['OAUTH_CLIENT_ID'],
     ENV['OAUTH_CLIENT_SECRET'],
     {:client_options => { :proxy => ENV["HTTP_PROXY"] || ENV["http_proxy"]  },name: "google_login", approval_prompt: '',:image_aspect_ratio => "square",
-      :image_size => 5, :ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'} }
+      :image_size => 5, ssl: {ca_file: Rails.root.join('lib/assets/cacert.pem').to_s} }
 
 
 end
