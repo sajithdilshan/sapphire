ad8c78b1ea78b59aaee71cd414f62ba90207af7d
Sajith Dilshan <sajithdilshan@gmail.com>
2013-08-31 13:40:51 +0530

    changed auth

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ OmniauthGoogleOauth2Example::Application.routes.draw do
   resources :feeds
   get "home/index"
   root :to => 'home#index'
-  match "/auth/google_login/callback" => "sessions#create"
-  match "/signout" => "sessions#destroy", :as => :signout
+  match "auth/google_login/callback" => "sessions#create"
+  match "signout" => "sessions#destroy", :as => :signout
   get "show_feed_list", to: "userfeeds#show_feed_list"
   get "mark_feed_viewed", to: "userfeeds#mark_feed_viewed"
   get "mark_feed_unread", to: "userfeeds#mark_feed_unread"
